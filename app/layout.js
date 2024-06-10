c70e125f46da4d9f70487eb5bcd5f3a129708eb6
Amrit <amritsinghal2211@gmail.com>
2024-06-10 11:03:20 +0530

    completed

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import SessionWrapper from "./component/SessionWrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SessionProvider>
       <body className={inter.className}>{children}</body>
+      </SessionProvider>
     </html>
   );
 }
